0eec441af5b5c41d764d732aa578a9ea9df9c57e
André <andre.spanguero@gmail.com>
2014-04-06 16:06:06 -0300

    Temos a foto agora #2

--- Ouvidoria/src/usp/ime/movel/ouvidoria/Registrar.java
+++ Ouvidoria/src/usp/ime/movel/ouvidoria/Registrar.java
@@ -1,15 +1,36 @@
 package usp.ime.movel.ouvidoria;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.crypto.spec.IvParameterSpec;
+
+import usp.ime.movel.ouvidoria.Login.AttemptLogin;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
-public class Registrar extends Activity {
+public class Registrar extends Activity implements OnClickListener {
 
 	private Intent intent;
 	private String username;
+	private Button mPicture;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,68 @@ public class Registrar extends Activity {
 		username = intent.getStringExtra("username");
 		TextView user = (TextView)findViewById(R.id.textView1);
 		user.setText("Usuário: " + username);
+		mPicture = (Button) findViewById(R.id.picture);
+		mPicture.setOnClickListener(this);
 	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.picture:
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			File f = new File(android.os.Environment.getExternalStorageDirectory(), "ouvidoria.jpg");
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+			System.out.println("Primeiro " + Uri.fromFile(f));
+			startActivityForResult(intent, 1);
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+                File f = new File(Environment.getExternalStorageDirectory()
+                        .toString() + "/ouvidoria.jpg");
+                System.out.println("Segundo " + Uri.fromFile(f));
+                try {
+                    Bitmap bm;
+                    bm = BitmapFactory.decodeFile(f.getAbsolutePath());
+                    if (bm == null) System.out.println("bm ta null");
+                    System.out.println(f.getAbsolutePath());
+                    bm = Bitmap.createScaledBitmap(bm, 140, 140, true);
+                    ImageView image = (ImageView) findViewById(R.id.ivImage);
+                    image.setImageBitmap(bm);
+ 
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream fOut = null;
+                    File file = new File(path, String.valueOf(System
+                            .currentTimeMillis()) + ".jpg");
+                    try {
+                        fOut = new FileOutputStream(file);
+                        bm.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+                        fOut.flush();
+                        fOut.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+             
+        }
+    }
 
 }
