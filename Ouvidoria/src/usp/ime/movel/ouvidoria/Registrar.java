7f1c702eb27bddea3dfe7db799245ad49b39e87b
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-04-17 18:14:09 -0300

    Tirei um import obsoleto

--- Ouvidoria/src/usp/ime/movel/ouvidoria/Registrar.java
+++ Ouvidoria/src/usp/ime/movel/ouvidoria/Registrar.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 import org.apache.http.entity.AbstractHttpEntity;
 import org.apache.http.entity.StringEntity;
