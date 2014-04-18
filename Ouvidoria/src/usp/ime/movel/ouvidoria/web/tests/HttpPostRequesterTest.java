f56632a39441f59998401c69c4e216f6e13e3ab7
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-04-18 19:51:35 -0300

    Usando mocks no onHttpResponse também.

--- Ouvidoria/src/usp/ime/movel/ouvidoria/web/tests/HttpPostRequesterTest.java
+++ Ouvidoria/src/usp/ime/movel/ouvidoria/web/tests/HttpPostRequesterTest.java
@@ -15,6 +15,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.AbstractHttpEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import static org.mockito.Mockito.*;
 
@@ -24,63 +26,65 @@ import usp.ime.movel.ouvidoria.web.HttpPostRequester;
 import usp.ime.movel.ouvidoria.web.OnHttpResponseListener;
 import android.test.UiThreadTest;
 
-public class HttpPostRequesterTest extends TestCase implements
-		OnHttpResponseListener {
+public class HttpPostRequesterTest extends TestCase {
 
+	// Mocks
 	private HttpPostRequester requester;
 	private HttpEntityProvider provider;
-	// Login login;
-	private CountDownLatch signal;
-	private HttpEntity inputEntity;
-	private String expectedResponse = "{\"ok\":true,\"nusp\":\"10001\",\"username\":\"teste\",\"email\":\"teste@example.com\"}";
+	private HttpEntity requestEntity;
 	private HttpClientFactory factory;
 	private HttpClient client;
+	private OnHttpResponseListener listener;
+
+	// Actual objects
+	private String expectedResponse;
+	private CountDownLatch signal;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 
-		inputEntity = mock(HttpEntity.class);
+		provider = mock(HttpEntityProvider.class);
+		requestEntity = mock(HttpEntity.class);
+		when(provider.provideEntity()).thenReturn(requestEntity);
+
 		factory = mock(HttpClientFactory.class);
 		client = mock(HttpClient.class);
 		when(factory.makeHttpClient()).thenReturn(client);
+
+		listener = mock(OnHttpResponseListener.class);
+
 		signal = new CountDownLatch(1);
-		provider = new HttpEntityProvider() {
-			public HttpEntity provideEntity() {
-				return inputEntity;
-			}
-		};
-		requester = new HttpPostRequester(this, factory, provider);
+
+		requester = new HttpPostRequester(listener, factory, provider);
 	}
 
 	@UiThreadTest
 	public void testPost() throws InterruptedException, JSONException,
 			ClientProtocolException, IOException {
 
+		expectedResponse = "{\"ok\":true,\"nusp\":\"10001\",\"username\":\"teste\",\"email\":\"teste@example.com\"}";
 		HttpResponse response = mock(HttpResponse.class);
 		HttpEntity responseEntity = mock(AbstractHttpEntity.class);
-		
+
 		when(response.getEntity()).thenReturn(responseEntity);
 		when(client.execute(isA(HttpPost.class))).thenReturn(response);
 		when(responseEntity.getContent()).thenReturn(
 				new ByteArrayInputStream(expectedResponse.getBytes()));
-		
+		doAnswer(new Answer<Object>() {
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				JSONObject expectedJSON = new JSONObject(expectedResponse);
+				JSONObject response = (JSONObject) invocation.getArguments()[0];
+				assertEquals(expectedJSON.toString(), response.toString());
+				signal.countDown();
+				return null;
+			}
+		}).when(listener).onHttpResponse(any(JSONObject.class));
+
 		requester.post("https://social.stoa.usp.br/plugin/stoa/authenticate/");
 		signal.await(30, TimeUnit.SECONDS);
 
 		assertEquals(0, signal.getCount());
-		
+		verify(provider).provideEntity();
 	}
-
-	@Override
-	public void onHttpResponse(JSONObject response) {
-		try {
-			JSONObject jsonObject = new JSONObject(expectedResponse);
-			assertEquals(jsonObject.toString(), response.toString());
-			signal.countDown();
-		} catch (JSONException e) {
-			assertTrue(false);
-			e.printStackTrace();
-		}
-	}
-
 }
