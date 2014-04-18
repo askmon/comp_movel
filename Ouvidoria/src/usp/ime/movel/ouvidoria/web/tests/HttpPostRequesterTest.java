07a6d9e00acc5fbf82888d1450558934c21b9ad8
Diego Araújo <diegoamc90@gmail.com>
2014-04-18 13:02:08 -0300

    Melhorei os testes

--- Ouvidoria/src/usp/ime/movel/ouvidoria/web/tests/HttpPostRequesterTest.java
+++ Ouvidoria/src/usp/ime/movel/ouvidoria/web/tests/HttpPostRequesterTest.java
@@ -1,66 +1,64 @@
 package usp.ime.movel.ouvidoria.web.tests;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import junit.framework.TestCase;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.entity.AbstractHttpEntity;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import usp.ime.movel.ouvidoria.Login;
 import usp.ime.movel.ouvidoria.web.HttpEntityProvider;
 import usp.ime.movel.ouvidoria.web.HttpPostRequester;
+import usp.ime.movel.ouvidoria.web.InsecureHttpClientFactory;
 import usp.ime.movel.ouvidoria.web.OnHttpResponseListener;
 import android.test.UiThreadTest;
 
 public class HttpPostRequesterTest extends TestCase implements OnHttpResponseListener {
 
-	private String string = "{\"incidentrecord\":{\"photo\":\"b64\",\"created_at\":\"2014-04-16T13:20:29-03:00\"," +
-			"\"latitude\":46.0,\"updated_at\":\"2014-04-16T13:20:29-03:00\",\"id\":61," +
-			"\"localization\":\"ime\",\"description\":\"alone in the dark 6\",\"longitude\":-23.0," +
-			"\"login\":\"\",\"status\":\"Aberto\",\"user\":8883723}}";
 	HttpPostRequester requester;
 	HttpEntityProvider provider;
+	Login login;
 	CountDownLatch signal;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
 		
+		login = new Login();
+		final ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("usp_id", "10001"));
+		params.add(new BasicNameValuePair("password", "claropassobluestrawmeuthorium"));
 		signal = new CountDownLatch(1);
 		provider = new HttpEntityProvider() {
-
 			public AbstractHttpEntity provideEntity() {
 				try {
-					return new StringEntity((new JSONObject(string)).toString());
+					return new UrlEncodedFormEntity(params);
 				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				return null;
 			}
-
-			public boolean hasContentType() {
-				return true;
-			}
-
-			public String getContentType() {
-				return "application/json";
-			}
 		};
-		requester = new HttpPostRequester(this, provider);
+		requester = new HttpPostRequester(login, new InsecureHttpClientFactory(), provider);
 	}
 	
 	@UiThreadTest
-	public void testPost() throws InterruptedException {
-		requester.post("http://uspservices.deusanyjunior.dj/incidente");
+	public void testPost() throws InterruptedException, JSONException {
+		String response = "{\"ok\":true,\"nusp\":\"10001\",\"username\":\"teste\",\"email\":\"teste@example.com\"}";
+		JSONObject jsonObject = new JSONObject(response);
+
+		requester.post("https://social.stoa.usp.br/plugin/stoa/authenticate/");
 		signal.await(30, TimeUnit.SECONDS);
-		
-		assertTrue("tenho que entender melhor o código", true);
+
+		assertTrue(login.getCheck());
+		assertEquals(login.getResponseMessage(), jsonObject.toString());
 	}
 	
 	@Override
