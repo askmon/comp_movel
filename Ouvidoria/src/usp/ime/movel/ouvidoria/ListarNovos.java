8fa3d98963981ed3299d9402a2a05e340b760b2b
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-04-20 13:59:38 -0300

    Usando arrays para manter a lista de incidentes.

--- Ouvidoria/src/usp/ime/movel/ouvidoria/ListarNovos.java
+++ Ouvidoria/src/usp/ime/movel/ouvidoria/ListarNovos.java
@@ -28,6 +28,9 @@ public class ListarNovos extends OuvidoriaActivity implements OnClickListener,
 	private boolean check = false;
 	private Intent intent;
 	private String status;
+	private int pageNumber;
+	private TextView[] incidentTexts;
+	private ImageView[] incidentImages;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +38,21 @@ public class ListarNovos extends OuvidoriaActivity implements OnClickListener,
 		setContentView(R.layout.listarnovos);
 		intent = getIntent();
 		status = "Carregando";
+		pageNumber = 0;
 		TextView stats = (TextView) findViewById(R.id.textView1);
 		stats.setText(status);
+		incidentTexts = new TextView[5];
+		incidentTexts[0] = (TextView) findViewById(R.id.ocorrencia1);
+		incidentTexts[1] = (TextView) findViewById(R.id.ocorrencia2);
+		incidentTexts[2] = (TextView) findViewById(R.id.ocorrencia3);
+		incidentTexts[3] = (TextView) findViewById(R.id.ocorrencia4);
+		incidentTexts[4] = (TextView) findViewById(R.id.ocorrencia5);
+		incidentImages = new ImageView[5];
+		incidentImages[0] = (ImageView) findViewById(R.id.ivImage1);
+		incidentImages[1] = (ImageView) findViewById(R.id.ivImage2);
+		incidentImages[2] = (ImageView) findViewById(R.id.ivImage3);
+		incidentImages[3] = (ImageView) findViewById(R.id.ivImage4);
+		incidentImages[4] = (ImageView) findViewById(R.id.ivImage5);
 		new HttpGetRequester().asyncGet(
 				"http://uspservices.deusanyjunior.dj/incidente/1.json", this);
 	}
@@ -48,33 +64,18 @@ public class ListarNovos extends OuvidoriaActivity implements OnClickListener,
 		try {
 			jsons = response.getJSONArray("incidentrecordlist");
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			TextView stats = (TextView) findViewById(R.id.textView1);
-			stats.setText("");
-			TextView oc1 = (TextView) findViewById(R.id.ocorrencia1);
-			TextView oc2 = (TextView) findViewById(R.id.ocorrencia2);
-			TextView oc3 = (TextView) findViewById(R.id.ocorrencia3);
-			TextView oc4 = (TextView) findViewById(R.id.ocorrencia4);
-			TextView oc5 = (TextView) findViewById(R.id.ocorrencia5);
-			ImageView im1 = (ImageView) findViewById(R.id.ivImage1);
-			ImageView im2 = (ImageView) findViewById(R.id.ivImage2);
-			ImageView im3 = (ImageView) findViewById(R.id.ivImage3);
-			ImageView im4 = (ImageView) findViewById(R.id.ivImage4);
-			ImageView im5 = (ImageView) findViewById(R.id.ivImage5);
-			oc1.setText(parseJSON(jsons, 0));
-			im1.setImageBitmap(BitmapFromFile(jsons, 0));
-			oc2.setText(parseJSON(jsons, 1));
-			im2.setImageBitmap(BitmapFromFile(jsons, 1));
-			oc3.setText(parseJSON(jsons, 2));
-			im3.setImageBitmap(BitmapFromFile(jsons, 2));
-			oc4.setText(parseJSON(jsons, 3));
-			im4.setImageBitmap(BitmapFromFile(jsons, 3));
-			oc5.setText(parseJSON(jsons, 4));
-			im5.setImageBitmap(BitmapFromFile(jsons, 4));
+			stats.setText("Incidentes " + 5 * pageNumber + " a "
+					+ (5 * (pageNumber + 1) - 1));
+			for (int i = 0; i < 5; i++) {
+				incidentTexts[i].setText(parseJSON(jsons, 5*pageNumber+i));
+				incidentImages[i].setImageBitmap(BitmapFromFile(jsons, 5*pageNumber+i));
+			}
 		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
