40df859ad53dce553cb14dafb8fd7e50b6580f3c
André <andre.spanguero@gmail.com>
2014-04-23 11:02:27 -0300

    Update ListarOcultos.java

--- Ouvidoria/src/usp/ime/movel/ouvidoria/ListarOcultos.java
+++ Ouvidoria/src/usp/ime/movel/ouvidoria/ListarOcultos.java
@@ -304,7 +304,7 @@ public class ListarOcultos extends OuvidoriaActivity implements OnClickListener,
 		case R.id.left:
 			pageNumber = Math.max(pageNumber - 1, 0);
 			onIncidenteUpdate();
-			j = j - 5;
+			j = 5 * pageNumber;
 			break;
 		case R.id.right:
 			pageNumber = Math.min(pageNumber + 1, updater.getIncidentes()
